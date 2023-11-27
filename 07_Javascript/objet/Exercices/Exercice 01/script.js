83bf659944b969696ec02ef3decac6449ca191eb
Anthony <an.hayart@gmail.com>
2023-11-27 21:05:22 +0100

    exercice js objet

--- 07_Javascript/objet/Exercices/Exercice 01/script.js
+++ 07_Javascript/objet/Exercices/Exercice 01/script.js
@@ -6,22 +6,46 @@
 
 // Pour ce faire, pensez à utiliser un sélecteur permettant d'atteindre l'élément de type `<tbody>` de votre tableau. Cet élément devra contenir, pour chaque personne, un `<tr>` possédant plusieurs `<td>` (un par attribut de la personne). 
 
-const newPersonne = {
-    firstname : "John",
-    lastname: "Dupont",
-    birthday: "22/02/1968"
+
+var personnes = [];
+
+function ajouterPersonne() {
+    var nom = document.getElementById("nom").value;
+    var prenom = document.getElementById("prenom").value;
+    var date = document.getElementById("date").value;
+
+    var personne = {
+        nom: nom,
+        prenom: prenom,
+        date: date
+    };
+
+    personnes.push(personne);
+
+    afficherTableau();
 }
 
-const firstname = document.querySelector("td#firstname")
-const lastname = document.querySelector("td#lastname")
-const birthday = document.querySelector("td#birthday")
+function afficherTableau() {
+    var tbody = document.querySelector("tbody");
 
-// document.querySelector("button#addPerson").addEventListener("click", () =>{
-//     const addTD = document.querySelector("td");
-//     if(addTD){
-//         const newTD = document.createElement("td");
-//         newTD.textContent = addTD.value
-//     }
-// })
+    tbody.innerHTML = "";
 
-console.log(newPersonne)
+
+    for (var i = 0; i < personnes.length; i++) {
+        var tr = document.createElement("tr");
+        var tdNom = document.createElement("td");
+        tdNom.textContent = personnes[i].nom;
+        tr.appendChild(tdNom);
+        var tdPrenom = document.createElement("td");
+        tdPrenom.textContent = personnes[i].prenom;
+        tr.appendChild(tdPrenom);
+
+        var tdDate = document.createElement("td");
+
+        tdDate.textContent = personnes[i].date;
+
+        tr.appendChild(tdDate);
+
+        tbody.appendChild(tr);
+    }
+}
