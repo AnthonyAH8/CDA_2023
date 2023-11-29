5fc3e7b17b08486aebb1529c9acd19f3743e7355
Anthony Hayart <an.hayart@gmail.com>
2023-11-29 16:46:55 +0100

    exercice 33

---
+++ 07_Javascript/objet/Exercices/Exercice_modal/cat.js
@@ -0,0 +1,13 @@
+export default class Chat{
+    name;
+    age;
+    race;
+    color;
+    
+    constructor(name, age, race, color){
+        this.name = name;
+        this.age = age;
+        this.race = race;
+        this.color = color;
+    }
+}
--- 07_Javascript/objet/Exercices/Exercice_modal/script.js
+++ 07_Javascript/objet/Exercices/Exercice_modal/script.js
@@ -1,3 +1,5 @@
+import Chat from "./cat.js"
+
 const modal = document.getElementById("modal")
 const validation = document.getElementById("modal-validation")
 const modalForm = document.getElementById("modal-form")
@@ -16,32 +18,40 @@ modalForm?.addEventListener("click", () =>{
     modal.style.display = "flex"
 })
 
-const cats = [
-    { name: "Garfield", age: 2, race: "Persian", color: "white" },
-    { name: "Toto", age: 4, race: "Siamois", color: "brown" },
-    { name: "Titi", age: 1, race: "Stray", color: "ginger" }
-];
+let cat =[];
 
-cats.forEach((cat) => {
-   
-    const TRCat = document.createElement("tr");
-    const TDname = document.createElement("td");
-    const TDage = document.createElement("td");
-    const TDrace = document.createElement("td");
-    const TDcolor = document.createElement("td");
-    const deleteTR = document.createElement("td");
-    const deleteCat = document.createElement("button");
+const monForm = document.getElementById("monForm")
+monForm.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    const newCat = new Chat(
+        document.getElementById("name").value,
+        document.getElementById("age").value,
+        document.getElementById("race").value,
+        document.getElementById("color").value
+        )
+    cat.push(newCat)
+
+
+const TRCat = document.createElement("tr");
+const TDname = document.createElement("td");
+const TDage = document.createElement("td");
+const TDrace = document.createElement("td");
+const TDcolor = document.createElement("td");
+const deleteTR = document.createElement("td");
+const deleteCat = document.createElement("button");
     
-    TDname.textContent = cat.name
-    TDage.textContent = cat.age
-    TDrace.textContent = cat.color
-    TDcolor.textContent = cat.race
+    TDname.textContent = newCat.name
+    TDage.textContent = newCat.age
+    TDrace.textContent = newCat.color
+    TDcolor.textContent = newCat.race
     deleteCat.textContent = "Enlever"
     deleteCat.addEventListener("click", () =>{
         TRCat.remove()
+        cat = cat.filter((chat) => chat !== newCat)
+        console.log(cat)
     })
     deleteTR.appendChild(deleteCat)
-    TRCat.appendChild(TDname);
+    TRCat.appendChild(TDname)
     TRCat.appendChild(TDage)
     TRCat.appendChild(TDrace)
     TRCat.appendChild(TDcolor)
@@ -49,20 +59,6 @@ cats.forEach((cat) => {
     
     document.getElementById("my-tbody")?.appendChild(TRCat)
     
-    const monForm = document.getElementById("monForm")
-    monForm.addEventListener("submit", (e) =>{
-        e.preventDefault();
-        const newCat = [
-            document.getElementById("name").value,
-            document.getElementById("age").value,
-            document.getElementById("race").value,
-            document.getElementById("color").value
-        ]
-        cats.push(newCat)
-    })
+    console.log(cat);
 
-    console.log(cats);
 })
-
-
-
