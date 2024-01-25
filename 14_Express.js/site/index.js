30bb8cd415627bb60d81b588304c415febb28b04
Anthony Hayart <an.hayart@gmail.com>
2024-01-25 17:22:03 +0100

    express.js

--- 14_Express.js/site/index.js
+++ 14_Express.js/site/index.js
@@ -1,23 +1,36 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+const path = require('path');
+const connection = require('./config/db');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/express_2000');
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Erreur de connexion à MongoDB :'));
+db.once('open', () => {
+  console.log('Connecté à MongoDB');
+});
 
 const router = require('./routes/bookRoutes');
 app.use('/', router);
+
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'));
 
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () =>{
     console.log(`Bienvenue sur mon serveur port ${PORT}`)
-})
+});
 
 app.get('/', (req, res) =>{
     res.send('Bienvenue sur mon site web')
 });
 
 app.get('/bienvenue', (req, res) =>{
-    res.send('<h1>Express 2000</h1>')
+    res.render('index', {title: 'Page d\'accueil', message: 'Bienvenue'})
 });
 
 app.get('/info', (req, res) => {
@@ -38,3 +51,4 @@ app.get('/redirection-accueil', (req, res) => {
 app.get('*', (req, res) => {
     res.send('Mauvaise direction')
 });
+
