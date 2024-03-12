ef72cfddb673fb07e1e2fc75b5f8dd3675fbe288
Anthony Hayart <an.hayart@gmail.com>
2024-03-12 10:59:06 +0100

    exercice react_native_shopping

--- 16-REACT_NATIVE/demo/components/ShoppingList.jsx
+++ 16-REACT_NATIVE/demo/components/ShoppingList.jsx
@@ -1,33 +1,60 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native'
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native'
 import React, { useState } from 'react'
+import ShoppingModal from './ShoppingModal'
 
-export default function ShoppingList() {
+export default function ShoppingList(props) {
 
-    const [shoppingCart, setShoppingCart] = useState(false)
+  const [modalVisible, setModalVisible] = useState(false);
+  const [shop, setShop] = useState([
+      { stuff: 'pommes' },
+      { stuff: 'poires' }
+  ]);
 
-    const shop = [
-        { stuff: 'pommes'},
-        { stuff: 'fricadelles'}
-    ]
+  function closeModal() {
+      setModalVisible(false);
+  }
+
+  function goToModal() {
+      setModalVisible(true);
+  }
+
+  function addNewFurniture(newItem) {
+      setShop([...shop, { stuff: newItem }]);
+      closeModal();
+  }
 
   return (
-    <View>
-      <Text>ShoppingList</Text>
-      <FlatList
-      data={shop}
-      renderItem={(s) => {
-        return(
-            <View>
-                <Text>{s.item.stuff}</Text>
-            </View>
-        )
-      }}
-      keyExtractor={(item, index) =>{
-        return index
-      }}
-      />
-    </View>
+      <View style={styles.furniture}>
+          <Text style={styles.title}>Ma liste de courses:</Text>
+          <ShoppingModal visible={modalVisible} closeModal={closeModal} addNewFurniture={addNewFurniture} />
+          <FlatList
+              data={shop}
+              renderItem={(s) => (
+                  <View>
+                      <Text>{s.item.stuff}</Text>
+                  </View>
+              )}
+              keyExtractor={(item, index) => index.toString()}
+              style={styles.liste}
+          />
+          <Button
+              title='Ajouter article'
+              onPress={goToModal}
+          />
+      </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  title:{
+    fontSize: 24
+  },
+  furniture: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  liste:{
+    marginBottom: 20
+  }
+})
---
+++ 16-REACT_NATIVE/demo/components/ShoppingModal.jsx
@@ -0,0 +1,60 @@
+import { View, Text, Modal, Button, StyleSheet, Image, TextInput } from 'react-native'
+import React, { useState } from 'react'
+
+export default function ShoppingModal(props) {
+    const [shoppingInput, setShoppingInput] = useState('');
+    const [shop, setShop] = useState([]);
+
+    function addNewFurniture() {
+            const newFurniture = { stuff: shoppingInput };
+            setShop([...shop, newFurniture]);
+            setShoppingInput('');
+    }
+
+    return (
+        <Modal visible={props.visible}>
+            <View style={styles.container}>
+                <Image
+                    style={styles.monImage}
+                    source={{ uri: 'https://cdn-icons-png.flaticon.com/512/6303/6303087.png' }}
+                />
+                <Text>Ma liste de courses:</Text>
+                <TextInput
+                    onChangeText={(text) => setShoppingInput(text)}
+                    value={shoppingInput}
+                    placeholder='Ajouter Course'
+                    style={styles.shoppingInput}
+                />
+                <Button
+                    title="Ajouter course"
+                    onPress={addNewFurniture}
+                />
+            </View>
+            <Button
+                title='Retour à la liste'
+                onPress={props.closeModal}
+            />
+        </Modal>
+    );
+}
+
+const styles = StyleSheet.create({
+    monImage: {
+        width: 100,
+        height: 100,
+    },
+    container: {
+        flex: 1,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    shoppingInput:{
+        margin: 20,
+        borderWidth: 1,
+        padding: 10,
+        width: 400,
+        textAlign: 'center',
+        borderRadius: 10,
+    }
+})
