a25d5c917a33bace984f374c84f3e606d4604c8c
Anthony Hayart <an.hayart@gmail.com>
2024-03-13 11:21:35 +0100

    exercice calculatrice

--- 16-REACT_NATIVE/demo/components/Calculator.jsx
+++ 16-REACT_NATIVE/demo/components/Calculator.jsx
@@ -1,35 +1,92 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { Button, Pressable, StyleSheet, Text, View } from 'react-native'
 import React, { useState } from 'react'
 
 export default function Calculator() {
-    const [input, setInput] = useState("")
     const [displayValue, setDisplayValue] = useState(0);
     const [operator, setOperator] = useState(null)
+    const [value, setValue] = useState("")
 
+    const numberInput = (number) => {
+        if (displayValue === '0') {
+            setDisplayValue(number.toString());
+        } else {
+            setDisplayValue(displayValue + number);
+        }
+    }
+
+    const answer = (newOperator) => {
+        setOperator(newOperator);
+        setValue(displayValue);
+        setDisplayValue('0');
+    };
+
+    const operation = () => {
+        const a = Number(value);
+        const b = Number(displayValue);
+    
+        if (operator === '+') {
+            setDisplayValue(a + b);
+        } else if (operator === '-') {
+            setDisplayValue(a - b);
+        } else if (operator === '*') {
+            setDisplayValue(a * b);
+        } else if (operator === '/') {
+            setDisplayValue(a / b);
+        } else if (operator === '^') {
+            setDisplayValue(Math.pow(a, b))
+        }
+    
+        setOperator(null);
+        setValue('');
+    };
+
+    const clear = () => {
+        setDisplayValue('0')
+        setOperator(null)
+        setValue('')
+    }
+    
+ 
     return (
         <View style={styles.container}>
                 <Text style={styles.title}>Calculatrice</Text>
+                <Text style={styles.result}>{displayValue}</Text>
             <View style={styles.main}>
-                <Button title='AC'/>
-                <Button title='^' />
-                <Button title='%' />
-                <Button title='/' />
-                <Button title='7' />
-                <Button title='8' />
-                <Button title='9' />
-                <Button title='*' />
-                <Button title='4' />
-                <Button title='5' />
-                <Button title='6' />
-                <Button title='-' />
-                <Button title='3' />
-                <Button title='2' />
-                <Button title='1' />
-                <Button title='+' />
-                <Button title='.' />
-                <Button title='0' />
-                <Button title='Del' />
-                <Button title='=' />
+                <Pressable onPress={clear}><Text style={styles.elements}>AC</Text></Pressable>
+                <Pressable
+                onPress={() => answer('^')}><Text style={styles.elements}>^</Text></Pressable>
+                <Pressable><Text style={styles.elements}>%</Text></Pressable>
+                <Pressable
+                onPress={() => answer('/')}><Text style={styles.elements}>/</Text></Pressable>
+                <Pressable
+                onPress={() => numberInput(7)}><Text style={styles.numbers}>7</Text></Pressable>
+                <Pressable
+                onPress={() => numberInput(8)}><Text style={styles.numbers}>8</Text></Pressable>
+                <Pressable
+                onPress={() => numberInput(9)}><Text style={styles.numbers}>9</Text></Pressable>
+                <Pressable
+                onPress={() => answer('*')}><Text style={styles.elements}>X</Text></Pressable>
+                <Pressable
+                onPress={() => numberInput(4)}><Text style={styles.numbers}>4</Text></Pressable>
+                <Pressable
+                onPress={() => numberInput(5)}><Text style={styles.numbers}>5</Text></Pressable>
+                <Pressable
+                onPress={() => numberInput(6)}><Text style={styles.numbers}>6</Text></Pressable>
+                <Pressable
+                onPress={() => answer('-')}><Text style={styles.elements}>-</Text></Pressable>
+                <Pressable
+                onPress={() => numberInput(1)}><Text style={styles.numbers}>1</Text></Pressable>
+                <Pressable
+                onPress={() => numberInput(2)}><Text style={styles.numbers}>2</Text></Pressable>
+                <Pressable
+                onPress={() => numberInput(3)}><Text style={styles.numbers}>3</Text></Pressable>
+                <Pressable
+                onPress={() => answer('+')}><Text style={styles.elements}>+</Text></Pressable>
+                <Pressable><Text style={styles.numbers}>.</Text></Pressable>
+                <Pressable><Text style={styles.numbers}>0</Text></Pressable>
+                <Pressable><Text style={styles.numbers}>Del</Text></Pressable>
+                <Pressable
+                onPress={operation}><Text style={styles.elements}>=</Text></Pressable>
             </View>
         </View>
     )
@@ -39,16 +96,55 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: 'black',
         flex: 1,
+        textAlign: 'center',
+        justifyContent: 'space-between',
     },
     title: {
         color: 'white',
         textAlign: 'center',
+        justifyContent: 'center',
         margin: 10,
         fontWeight: 'bold',
         fontSize: 24
     },
     main:{
+        display: 'flex',
         flexWrap: 'wrap',
-        flexDirection: 'row'
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    elements:{
+        display: 'flex',
+        color: 'white',
+        backgroundColor: 'grey',
+        width: 100,
+        height: 100,
+        textAlign: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 20,
+        padding: 32,
+        margin: 5,
+        fontWeight: 'bold',
+        fontSize: 24
+    },
+    numbers:{
+        backgroundColor: 'white',
+        width: 100,
+        height: 100,
+        textAlign: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        display: 'flex',
+        borderRadius: 50,
+        padding: 32,
+        margin: 5,
+        fontWeight: 'bold',
+        fontSize: 24
+    },
+    result:{
+        color: 'lightgrey',
+        textAlign: 'right',
+        fontSize: 60
     }
     })
