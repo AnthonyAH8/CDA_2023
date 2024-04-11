fb2a238dfe678151dc383652792ac6940b17341b
Anthony Hayart <an.hayart@gmail.com>
2024-04-11 17:29:23 +0200

    plante arbre

--- 17_JAVA/first_project/src/main/java/org/example/exercice_heritage/Arbre.java
+++ 17_JAVA/first_project/src/main/java/org/example/exercice_heritage/Arbre.java
@@ -2,4 +2,16 @@ package org.example.exercice_heritage;
 
 public class Arbre extends Plante{
 
+    public Arbre(String name, double height, String color) {
+        super(name, height, color);
+        this.circonference = circonference;
+
+        public int getCirconference(){
+            return circonference;
+        }
+
+        public void setCirconference(int circonference) {
+            this.circonference = circonference;
+        }
+    }
 }
