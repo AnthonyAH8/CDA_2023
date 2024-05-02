ff140f930a761be183bfe00b15f621847e866026
Anthony Hayart <an.hayart@gmail.com>
2024-05-02 11:43:20 +0200

    exercice strategy tax

--- 17_JAVA/first_project/src/main/java/org/example/exercice_strategy/Main.java
+++ 17_JAVA/first_project/src/main/java/org/example/exercice_strategy/Main.java
@@ -5,13 +5,15 @@ public class Main {
 
         NavigationSystem navigationSystem = new NavigationSystem();
 
-
         navigationSystem.setStrategy(new RoadStrategy());
         navigationSystem.navigate(" vers Paris.");
 
 
         navigationSystem.setStrategy(new OffRoadStrategy());
         navigationSystem.navigate(" dans les montagnes.");
+
+        navigationSystem.setStrategy(new EconomicStrategy());
+        navigationSystem.navigate(" vers la plage.");
 
         navigationSystem.setStrategy(new EconomicStrategy());
         navigationSystem.navigate(" vers la plage.");
