f8f7c5aa852b4c6230e93da2b59350c78ad686b3
Anthony Hayart <an.hayart@gmail.com>
2024-07-10 09:18:18 +0200

    wip exercice rh

--- Spring/tp_rh_spring/src/main/java/com/example/tp_rh_spring/controller/CandidateController.java
+++ Spring/tp_rh_spring/src/main/java/com/example/tp_rh_spring/controller/CandidateController.java
@@ -1,16 +1,17 @@
 package com.example.tp_rh_spring.controller;
 
 import com.example.tp_rh_spring.dto.candidate.CandidateDtoGet;
+import com.example.tp_rh_spring.dto.candidate.CandidateDtoPost;
 import com.example.tp_rh_spring.service.CandidateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/rh")
 public class CandidateController {
@@ -28,5 +29,19 @@ public class CandidateController {
         return ResponseEntity.ok(candidateService.findById(id));
     }
 
-    //TODO
+    @PostMapping("/candidates/add")
+    public ResponseEntity<CandidateDtoGet> addCandidate(@Validated @RequestBody CandidateDtoPost candidateDtoPost) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(candidateService.create(candidateDtoPost));
+    }
+
+    @PutMapping("/candidates/update/{id}")
+    public ResponseEntity<CandidateDtoGet> updateCandidate(@PathVariable int id, @Validated @RequestBody CandidateDtoPost candidateDtoPost) {
+        return ResponseEntity.ok(candidateService.update(id, candidateDtoPost));
+    }
+
+    @DeleteMapping("/candidates/delete/{id}")
+    public ResponseEntity<String> deleteCandidate(@PathVariable int id) {
+        candidateService.delete(id);
+        return ResponseEntity.ok("Candidat " + id + "supprimé");
+    }
 }
